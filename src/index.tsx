121ad6a9ff4b3338b896a1ad0ec521a1575e0f49
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-28 10:27:44 +0530

    Updated file structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import reportWebVitals from './reportWebVitals';
 import { registerLicense } from '@syncfusion/ej2-base';
 registerLicense("Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCfEx0Q3xbf1x0ZFBMYV1bQHdPIiBoS35RckViW3hfd3VUQmJbUU12")
 
